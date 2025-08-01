f3a245da12e569162e2716f940c7f2996eb1f13e
omar mazen <86468135+omar-mazen@users.noreply.github.com>
2025-08-02 01:19:03 +0300

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
-console.log(supabaseKey, supabaseUrl);
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
