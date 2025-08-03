123c399c4b1c3451edd11305e4551018d8e8f508
omar mazen <86468135+omar-mazen@users.noreply.github.com>
2025-08-04 02:18:43 +0300

    Update uploadSampleData.js

--- scripts/uploadSampleData.js
+++ scripts/uploadSampleData.js
@@ -507,8 +507,8 @@ const bookings = [
     countryFlag: 'https://flagcdn.com/tw.svg',
   },
 ];
-const supabaseUrl = process.env.NEXT_PUBLIC.SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC.SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function deleteGuests() {
